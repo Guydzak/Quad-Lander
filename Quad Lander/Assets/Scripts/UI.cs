dbce25902e2665a35aaee1ab9a6a53ae0ce08b25
Christopher Mardiryan <chrismardiryan@yahoo.com>
2020-03-04 15:59:25 +1100

    Tweaked some coding.

---
+++ Quad Lander/Assets/Scripts/UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+
+    public float timeRemaining = 45.0f; //How much time is remaining.
+    public Text timeText; //References the "Time Remaining" UI.
+
+    private void Update()
+    {
+        timeRemaining -= Time.deltaTime; //Subtracting "Time Remaining" from delta time.
+        timeText.text = "Time Remaining: " + timeRemaining; // The text represents the time remaining. These two variables are directly conntected.
+
+        if(timeRemaining <= 0) //When the time reaches zero, "Game Over is activated.
+        {
+            GameOver();
+        }
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+
+
+}
+
+
