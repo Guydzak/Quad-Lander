dde76f1613e7123a5fb9b596a535d5d4384e836e
Alex Gubieski <1010524@student.sae.edu.au>
2020-03-09 01:05:17 +1100

    Thrust Script Implemented

---
+++ Quad Lander/Assets/Scripts/Thrust.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thrust();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed; 
+    }
+
+    void thrust()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 0.75f), ForceMode2D.Impulse);
+        }
+    }
+}
