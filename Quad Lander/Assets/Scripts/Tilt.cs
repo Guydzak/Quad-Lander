60f5b448339fd4c058370d7b8cbe8b8ef7008e7e
Alex Gubieski <1010524@student.sae.edu.au>
2020-03-09 02:32:40 +1100

    Tilt Implentation

---
+++ Quad Lander/Assets/Scripts/Tilt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float tiltX;
+    public float speed = 10;
+    public float edgeleft = -10f;
+    public float edgeRight = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiltX = Input.acceleration.x * speed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, edgeleft, edgeRight), transform.position.y);
+    }
+}
